3c21cb4a969ab6df685740e25e1c32f4fabd02d1
cheddes <sivakumar.chedde@gmail.com>
2016-01-01 22:40:34 +0530

    initial content

---
+++ selenium-automation/src/test/java/selenium/automation/test/TestAEMDemoPage.java
@@ -0,0 +1,43 @@
+package selenium.automation.test;
+
+import java.util.concurrent.TimeUnit;
+import org.junit.*;
+import static org.junit.Assert.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class TestAEMDemoPage {
+  private WebDriver driver;
+  private String baseUrl;
+  //private boolean acceptNextAlert = true;
+  private StringBuffer verificationErrors = new StringBuffer();
+
+  @Before
+  public void setUp() throws Exception {
+    //driver = new FirefoxDriver();
+	driver = new ChromeDriver();
+    baseUrl = "http://localhost:4502";
+    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+  }
+
+  @Test
+  public void validateHomePage() throws Exception {
+    driver.get(baseUrl + "/libs/granite/core/content/login.html?resource=%2Fcontent%2Faemdemo%2Fen.html&$$login$$=%24%24login%24%24");
+    driver.findElement(By.id("username")).clear();
+    driver.findElement(By.id("username")).sendKeys("admin");
+    driver.findElement(By.id("password")).clear();
+    driver.findElement(By.id("password")).sendKeys("admin");
+    driver.findElement(By.cssSelector("button.coral-Button.coral-Button--primary")).click();
+    driver.findElement(By.linkText("Français")).click();
+    driver.findElement(By.linkText("English")).click();
+  }
+
+  @After
+  public void tearDown() throws Exception {
+    driver.quit();
+    String verificationErrorString = verificationErrors.toString();
+    if (!"".equals(verificationErrorString)) {
+      fail(verificationErrorString);
+    }
+  }
+}
